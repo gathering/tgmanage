f87909d04cb0919e80d825207e09107abfcfa0c3
root <root@frank.tg14.gathering.org>
2014-04-16 16:45:02 +0200

    Adjustments for nms-public for TG14.

--- clients/update-public-nms.sh
+++ clients/update-public-nms.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-DIR=/srv/www/nms-public.tg13.gathering.org
+DIR=/root/tgmanage/web/nms-public.gathering.org
 
-wget -qO$DIR/nettkart-trafikk.png.new http://nms.tg13.gathering.org/nettkart.pl
-wget -qO$DIR/nettkart-dhcp.png.new http://nms.tg13.gathering.org/dhcpkart.pl
-wget -qO$DIR/led.txt.new http://nms.tg13.gathering.org/led.pl
+wget -qO$DIR/nettkart-trafikk.png.new http://nms.tg14.gathering.org/nettkart.pl
+wget -qO$DIR/nettkart-dhcp.png.new http://nms.tg14.gathering.org/dhcpkart.pl
+wget -qO$DIR/led.txt.new http://nms.tg14.gathering.org/led.pl
 mv $DIR/nettkart-trafikk.png.new $DIR/nettkart-trafikk.png
 mv $DIR/nettkart-dhcp.png.new $DIR/nettkart-dhcp.png
 mv $DIR/led.txt.new $DIR/led.txt
 
-/usr/bin/perl -i -pe 'use POSIX qw(strftime); my $timestamp = strftime("%a, %d %b %Y %H:%M:%S %z", localtime(time())); s/Sist oppdatert:.*/Sist oppdatert: $timestamp/g;' /srv/www/nms-public.tg13.gathering.org/dhcp.html
-/usr/bin/perl -i -pe 'use POSIX qw(strftime); my $timestamp = strftime("%a, %d %b %Y %H:%M:%S %z", localtime(time())); s/Sist oppdatert:.*/Sist oppdatert: $timestamp/g;' /srv/www/nms-public.tg13.gathering.org/trafikk.html
+/usr/bin/perl -i -pe 'use POSIX qw(strftime); my $timestamp = strftime("%a, %d %b %Y %H:%M:%S %z", localtime(time())); s/Sist oppdatert:.*/Sist oppdatert: $timestamp/g;' $DIR/dhcp.html
+/usr/bin/perl -i -pe 'use POSIX qw(strftime); my $timestamp = strftime("%a, %d %b %Y %H:%M:%S %z", localtime(time())); s/Sist oppdatert:.*/Sist oppdatert: $timestamp/g;' $DIR/trafikk.html
