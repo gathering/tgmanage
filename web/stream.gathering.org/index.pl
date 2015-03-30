31a664b20ba24e1b3348d2bf51cb89fc380b45ea
root <root@einstein.tg15.gathering.org>
2015-03-30 22:24:29 +0200

    Anton: Cleaned up the folderstructure a bit, also reworked the front page with some CSS, auto-play, video source swap (al tho not inline, but hey), some config parameters as well as some todo-removes if we dont need the conf.

--- web/stream.gathering.org/index.pl
+++ web/stream.gathering.org/index.pl
@@ -2,13 +2,16 @@
 use warnings;
 use strict;
 use CGI;
+# apt-get install libgeo-ip-perl
 use Geo::IP;
 use NetAddr::IP;
 use Net::IP;
 # apt-get install libnet-ip-perl libnetaddr-ip-perl 
 use HTML::Template;
+# apt-get install libhtml-template-perl
 use stream;
 use stream::config;
+use MIME::Base64;
 
 my $client = CGI->new;
 
@@ -16,6 +19,7 @@ my $v4net = $stream::config::v4net;
 my $v6net = $stream::config::v6net;
 my $tg = $stream::config::tg;
 my $tg_full = $stream::config::tg_full;
+my $video_url = $stream::config::video_url;
 my %streams = %stream::config::streams;
 
 my $force_unicast = $client->param('forceunicast');
@@ -29,44 +33,57 @@ my $clip = $client->remote_addr();
 my $template = HTML::Template->new(filename => 'index.tmpl');
 my $is_local = &is_ip_local($clip, $v4net, $v6net);
 
-my @streams = &html_local_test();
+my @streams = &loop_webcams("event");
+my @camstreams = &loop_webcams("camera");
+
+my %input;
+for my $key ( $client->param() ) {
+	$input{$key} = $client->param($key);
+}
+
 $template->param(TG => $tg);
 $template->param(TG_FULL => $tg_full);
 $template->param(STREAMS => \@streams);
+$template->param(CAMSTREAMS => \@camstreams);
 $template->param(NOHEADER => $no_header);
+if(exists $input{url}) {
+	$template->param(VIDEO_URL => decode_base64($input{url}));
+	$template->param(VIDEO_AUTO_PLAY => 'true');
+} else {
+	$template->param(VIDEO_URL => $video_url);
+	$template->param(VIDEO_AUTO_PLAY => 'false');
+}
 print $template->output();
 
 
-sub html_local_test() {
+sub loop_webcams() {
 	my @s = ();
 	foreach my $name (sort { $streams{$a}->{priority} <=> $streams{$b}->{priority} } keys %streams) {
-		my $title_link = "http://stream.tg$tg.gathering.org/stream.pl?delivery=%s&stream=${name}&interlaced=%s";
-		my $multicast_link = $streams{$name}->{has_multicast} ? "multicast" : "unicast";
-		$multicast_link = "unicast" if ($force_unicast == 1 || not $is_local);
+		if ($streams{$name}->{type} eq $_[0] && $streams{$name}->{online}) {
+			my $title_link = "http://stream.tg$tg.gathering.org/stream.pl?delivery=%s&stream=${name}&interlaced=%s";
+			my $multicast_link = $streams{$name}->{has_multicast} ? "multicast" : "unicast";
+			$multicast_link = "unicast" if ($force_unicast == 1 || not $is_local);
 
-		if ($streams{$name}->{external}) {
-			 $title_link = $streams{$name}->{url};
-		} else {
-			$title_link = sprintf($title_link, $multicast_link, $streams{$name}->{interlaced});
+			if ($streams{$name}->{external}) {
+				 $title_link = $streams{$name}->{url};
+			} else {
+				$title_link = sprintf($title_link, $multicast_link, $streams{$name}->{interlaced});
+			}
+			my %hash = (
+				'title_link' => $title_link,
+				'title' => $streams{$name}->{title},
+				'quality' => $streams{$name}->{quality},
+				'type' => $streams{$name}->{type},
+			);
+			if ($multicast_link eq "multicast") {
+				$hash{'is_multicast'} .= 1; 
+				my $unicast_link = $title_link;
+				$unicast_link=~s/multicast/unicast/g;
+				$hash{'unicast_link'} .= $unicast_link;
+			}
+			$hash{'description'} .= $streams{$name}->{description} if exists($streams{$name}->{description});
+			push(@s, \%hash);
 		}
-		my %hash = (
-			'title_link' => $title_link,
-			'title' => $streams{$name}->{title},
-			'source' => $streams{$name}->{source},
-			'quality' => $streams{$name}->{quality},
-			'location' => $streams{$name}->{location},
-			'type' => $streams{$name}->{type},
-			'delivery' => $multicast_link,
-		);
-		if ($multicast_link eq "multicast") {
-			$hash{'is_multicast'} .= 1; 
-			my $unicast_link = $title_link;
-			$unicast_link=~s/multicast/unicast/g;
-			$hash{'unicast_link'} .= $unicast_link;
-		}
-		$hash{'description'} .= $streams{$name}->{description} if exists($streams{$name}->{description});
-		push(@s, \%hash);
-
 	}
 	return @s;
 }
