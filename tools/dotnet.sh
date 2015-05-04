5108aa44f5c8a1accf42c31cb9670e5dc87ec8a0
Kristian Lyngstol <kristian@bohemians.org>
2015-05-04 10:37:07 +0200

    Bump dotnet.sh

--- tools/dotnet.sh
+++ tools/dotnet.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 DATE="$(date +%s)"
+if [ -z "$1" ] || [ -z "$2" ]; then
+	echo "Usage: $0 <ip> <community>"
+	exit 1;
+fi
 ./lldpdiscover.pl $1 $2 | ./draw-neighbors.pl | dot -Tpng > ${DATE}.png
+echo File name: ${DATE}.png
