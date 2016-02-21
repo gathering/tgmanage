db229dc6ee85e11ec0093ac7e8d3547ce9bbe3b9
Kristian Lyngstol <kly@kly.no>
2016-02-21 21:20:15 +0000

    nms: Convert ping to new format
    Also: Breaks front-end because I changed the data format.
    
    I want 'when'-logic to be global, which means standardizing on a single
    column name for time stamps. "Updated" made no sense anyway.

--- web/nms.gathering.org/ping.pl
+++ web/nms.gathering.org/ping.pl
@@ -1,39 +1,25 @@
 #! /usr/bin/perl
-use CGI;
-use DBI;
-use JSON::XS;
 use lib '../../include';
-use nms;
-my $cgi = CGI->new;
+use nms::web;
 
-my $dbh = nms::db_connect();
-
-my $now = "now()";
-if ($cgi->param('now') != undef) {
-	$now = "'" . $cgi->param('now') . "'::timestamp ";
-}
-my $when =" updated > " . $now . " - '15 secs'::interval and updated < " . $now . " ";
-
-my %json = ();
-
-my $q = $dbh->prepare("SELECT DISTINCT ON (sysname) updated,sysname, latency_ms FROM ping NATURAL JOIN switches WHERE updated in (select max(updated) from ping where $when group by switch)");
+my $q = $nms::web::dbh->prepare("SELECT DISTINCT ON (sysname) time,sysname, latency_ms FROM ping NATURAL JOIN switches WHERE time in (select max(time) from ping where " . $nms::web::when . " group by switch)");
 $q->execute();
 while (my $ref = $q->fetchrow_hashref()) {
-	$json{'switches'}{$ref->{'sysname'}}{'latency'} = $ref->{'latency_ms'};
+	$nms::web::json{'switches'}{$ref->{'sysname'}}{'latency'} = $ref->{'latency_ms'};
 }
 
-my $qs = $dbh->prepare("SELECT DISTINCT ON (switch) switch, latency_ms FROM ping_secondary_ip WHERE $when ORDER BY switch, updated DESC;");
+my $qs = $nms::web::dbh->prepare("SELECT DISTINCT ON (switch) switch, latency_ms FROM ping_secondary_ip WHERE " . $nms::web::when .  " ORDER BY switch, time DESC;");
 $qs->execute();
 while (my $ref = $qs->fetchrow_hashref()) {
-	$json{'switches'}{$ref->{'switch'}}{'latency_secondary'} = $ref->{'latency_ms'};
+	$nms::web::json{'switches'}{$ref->{'switch'}}{'latency_secondary'} = $ref->{'latency_ms'};
 }
 
-my $lq = $dbh->prepare("SELECT DISTINCT ON (linknet) linknet, latency1_ms, latency2_ms FROM linknet_ping WHERE $when ORDER BY linknet, updated DESC;");
+my $lq = $nms::web::dbh->prepare("SELECT DISTINCT ON (linknet) linknet, latency1_ms, latency2_ms FROM linknet_ping WHERE ". $nms::web::when . " ORDER BY linknet, time DESC;");
 $lq->execute();
 while (my $ref = $lq->fetchrow_hashref()) {
-	$json{'linknets'}{$ref->{'linknet'}} = [ $ref->{'latency1_ms'}, $ref->{'latency2_ms'} ];
+	$nms::web::json{'linknets'}{$ref->{'linknet'}} = [ $ref->{'latency1_ms'}, $ref->{'latency2_ms'} ];
 }
 
 $q->execute();
-print $cgi->header(-type=>'text/json; charset=utf-8');
-print JSON::XS::encode_json(\%json);
+
+finalize_output();
