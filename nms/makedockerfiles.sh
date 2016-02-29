db833dca13ddbc95e8e3a9514e94bf534d06e4db
Magnus Kirø <magnuskiro@gmail.com>
2016-02-29 19:46:56 +0100

    tabs to spaces!

--- nms/makedockerfiles.sh
+++ nms/makedockerfiles.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 awk -v TARGET=/dev/null '
 BEGIN {
-	system("mkdir -p build")
+    system("mkdir -p build")
 }
 /^@template/ {
-	TARGET="build/Dockerfile."$2;
-	TARGETS[ntargets++]=$2;
-	print "# Generated" > TARGET
-	next;
+    TARGET="build/Dockerfile."$2;
+    TARGETS[ntargets++]=$2;
+    print "# Generated" > TARGET
+    next;
 }
 {
-	print > TARGET
+    print > TARGET
 }
 END {
-	print "#! /bin/sh" > "build/build.sh"
-	print "set -e" > "build/build.sh"
-	for (x in TARGETS) {
-		a = TARGETS[x]
-		print "echo Building "a > "build/build.sh"
-		print "echo Logs: build/"a".log" > "build/build.sh"
-		print "docker build -t "a" -f build/Dockerfile."a" . > build/"a".log 2>&1 || cat build/"a".log" > "build/build.sh"
-	}
-	system("chmod +x build/build.sh")
+    print "#! /bin/sh" > "build/build.sh"
+    print "set -e" > "build/build.sh"
+    for (x in TARGETS) {
+        a = TARGETS[x]
+        print "echo Building "a > "build/build.sh"
+        print "echo Logs: build/"a".log" > "build/build.sh"
+        print "docker build -t "a" -f build/Dockerfile."a" . > build/"a".log 2>&1 || cat build/"a".log" > "build/build.sh"
+    }
+    system("chmod +x build/build.sh")
 }' < Dockerfile.in
 build/build.sh
