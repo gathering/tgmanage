c70e294a0425a7d417bcf41677a55970e9e59489
Nicolai Tellefsen <niccofyren@gmail.com>
2016-03-22 20:34:56 +0100

    NMS: Make sure switch redraw also redraws infotext

--- web/nms.gathering.org/js/nms-map.js
+++ web/nms.gathering.org/js/nms-map.js
@@ -273,6 +273,9 @@ nmsMap._drawSwitch = function(sw)
 	this._drawBox(this._c.switch.ctx, box['x'],box['y'],box['width'],box['height']);
 	this._c.switch.ctx.shadowBlur = 0;
 	this._drawText(this._c.text.ctx, sw,box);
+
+	if(this._info[sw])
+		this._drawSwitchInfo(sw);
 };
 
 nmsMap._drawSwitchInfo = function(sw) {
