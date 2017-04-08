edd1d4dab1a00cce69440e06dfb932a7185960c0
Joachim Tingvold <joachim@tingvold.com>
2017-04-08 14:20:44 +0200

    Some Juniper switches won't send vendor-class-identifier...

--- bootstrap/make-dhcpd.pl
+++ bootstrap/make-dhcpd.pl
@@ -206,10 +206,16 @@ option option-150 code 150 = { ip-address };
 option vendor-class-identifier code 60 = text;
 
 # only allow FAP "clients"
-class "FAP" {
+class "fap-vendor-class" {
 	# Vendor-Class Option 60, length 21: "Juniper-ex2200-48t-4g"
+	# Vendor-Class Option 60, length 21: "Juniper-ex3300-48p"
 	match if substring (option vendor-class-identifier, 0, 10) = "Juniper-ex";
-	log( info, concat( "FAP: " , option vendor-class-identifier , " - " , hardware , " (" , option host-name , ") - " , option agent.circuit-id )); 
+	log( info, concat( "FAP: " , option vendor-class-identifier , " - " , hardware , " (" , option host-name , ") - " , option agent.circuit-id ));
+}
+class "fap-mac" {
+	# some Juniper switches won't send vendor-class-identifier
+	match if binary-to-ascii(16,8,":",substring(hardware, 1, 3)) = "44:f4:77";
+	log( info, concat( "FAP: " , hardware , " (" , option host-name , ") - " , option agent.circuit-id ));
 }
 
 group {
@@ -253,7 +259,8 @@ EOF
 	        option routers $fap_gw;
 		pool {
 		        range $fap_first $fap_last;
-			allow members of "FAP";
+			allow members of "fap-vendor-class";
+			allow members of "fap-mac";
 		}
 	}
 EOF
