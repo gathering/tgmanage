f401f99fbdf3595301871e6101616bf1d541463c
larsstrandos <lars@spanning.no>
2026-03-26 21:51:16 +0100

    Added Arista ZTP to ZTPrince

--- tools/ztprince/config.py
+++ tools/ztprince/config.py
@@ -1,9 +1,17 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings,SettingsConfigDict
 
 
 class Settings(BaseSettings):
     app_name: str = "ZTPrince"
     netbox_url: str
     netbox_token: str
+    cvAddr: str
+    enrollmentToken: str
+    ntpServer: str
+
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8'
+    )
 
 settings = Settings()
--- tools/ztprince/main.py
+++ tools/ztprince/main.py
@@ -2,17 +2,23 @@ import pynetbox
 
 from fastapi import FastAPI, Response
 from fastapi.responses import PlainTextResponse
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+env = Environment(
+    loader=PackageLoader("ztprince"),
+    autoescape=select_autoescape()
+)
 
 from .config import settings
 
 
 app = FastAPI()
 
-nb = pynetbox.api(
-    settings.netbox_url,
-    token=settings.netbox_token,
-    threading=True,
-)
+#nb = pynetbox.api(
+#    settings.netbox_url,
+#    token=settings.netbox_token,
+#    threading=True,
+#)
 
 
 @app.get("/")
@@ -28,3 +34,10 @@ def read_item(hostname: str, response: Response):
         return
     config = device.render_config.create()
     return PlainTextResponse(content=config['content'])
+
+@app.get("/arista-ztp/")
+def read_item(response: Response):
+    bootstrap = env.get_template("bootstrap_template.j2")
+    return PlainTextResponse(content=bootstrap.render(cvAddr=settings.cvAddr,
+        enrollmentToken=settings.enrollmentToken,
+        ntpServer=settings.ntpServer))
