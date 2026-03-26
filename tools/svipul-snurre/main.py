9ef339994ea2426050e0bb23c2efbabaf449b05c
Håkon Solbjørg <hakon@solbj.org>
2026-03-27 00:23:34 +0100

    svipul-snurre: Fetch system and ports

--- tools/svipul-snurre/main.py
+++ tools/svipul-snurre/main.py
@@ -17,13 +17,15 @@ rabbit = pika.BlockingConnection(pika.ConnectionParameters(settings.broker_url))
 rabbit_channel = rabbit.channel()
 
 
-def create_order(target: str, mode="Get", oids=['sysName.0']):
-    id = fastuuid.uuid7()
+def create_order(target: str, id=None, mode="Get", oids=['sysName.0'], elements=[]):
+    if id is None:
+        id = fastuuid.uuid7()
     return {
         'target': target,
         'mode': mode,
         'oids': oids,
         'id': id,
+        'elements': elements,
     }
 
 def send_order(order):
@@ -34,27 +36,41 @@ def main():
     print("Hello from svipul-snurre!")
 
     while True:
-        orders = []
-
         devices = nb.dcim.devices.filter(status="active")
         for device in devices:
             if not device.primary_ip:
                 logger.debug(f'Skipping {device.name} because it has no primary ip')
                 continue
+            addrs = []
             if device.primary_ip4:
                 addr = netaddr.IPNetwork(device.primary_ip4.address)
-                send_order(create_order(addr.ip))
+                addrs.append(addr)
             if device.primary_ip6:
                 addr = netaddr.IPNetwork(device.primary_ip6.address)
-                send_order(create_order(addr.ip))
+                addrs.append(addr)
+
+            for addr in addrs:
+                send_order(create_order(addr.ip, id='system', mode='Get', oids=system_oids))
+                send_order(create_order(addr.ip, id='ports', mode='GetElements', oids=ports_oids, elements=['.*']))
 
             print("poll", device)
 
 
         time.sleep(10)
 
-    rabbit.close()
 
+ports_oids = ["ifAdminStatus", "ifDescr", "ifInDiscards", "ifInErrors", "ifInNUcastPkts", "ifInOctets", "ifInUcastPkts", "ifInUnknownProtos", "ifIndex", "ifLastChange", "ifMtu", "ifOperStatus", "ifOutDiscards", "ifOutErrors", "ifOutNUcastPkts", "ifOutOctets", "ifOutQLen", "ifOutUcastPkts", "ifPhysAddress", "ifSpecific", "ifSpeed", "ifType"]
+system_oids = [
+    "sysUpTime.0",
+    "sysName.0",
+    "sysDescr.0",
+    "entPhysicalSerialNum.0",
+    "sysUpTime.0",
+    "sysName.0",
+    "sysDescr.0",
+    "entPhysicalSerialNum.0",
+]
 
 if __name__ == "__main__":
     main()
+    rabbit.close()
