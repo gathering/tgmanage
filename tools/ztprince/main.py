aad4d6be6215b9a1ec8b24723a837bc5829b5355
larsstrandos <lars@spanning.no>
2026-03-27 23:00:50 +0100

    Remove some comments added under testing

--- tools/ztprince/main.py
+++ tools/ztprince/main.py
@@ -15,11 +15,11 @@ from config import settings
 
 app = FastAPI()
 
-#nb = pynetbox.api(
-#    settings.netbox_url,
-#    token=settings.netbox_token,
-#    threading=True,
-#)
+nb = pynetbox.api(
+    settings.netbox_url,
+    token=settings.netbox_token,
+    threading=True,
+)
 
 
 @app.get("/")
